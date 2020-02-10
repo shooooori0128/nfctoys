fd0d6438a19fd7d3bee9eca75f39c0368efcb7fd
shooooori0128 <pf.shori0128@gmail.com>
2020-02-10 21:04:35 +0900

    とりあえず動作できる状態に戻し

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
-  get '/', to: 'frames#index'
-
+  get 'frames/index'
   ActiveAdmin.routes(self)
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
