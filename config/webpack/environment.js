3451ef89baf11e85c92ad3387359f6dbd95d8e04
shooooori0128 <pf.shori0128@gmail.com>
2020-02-15 23:29:08 +0900

    vueのstyleが適用されない問題の解消

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -6,7 +6,4 @@ const vue = require('./loaders/vue')
 environment.plugins.prepend('VueLoaderPlugin', new VueLoaderPlugin())
 environment.loaders.prepend('vue', vue)
 environment.loaders.prepend('typescript', typescript)
-// Vueのstyleが適用されない問題の解消
-// 参考 => https://qiita.com/tocomi/items/0e1522418d0a21a6b291
-environment.loaders.get('vue').use[0].options.extractCSS = false
 module.exports = environment
